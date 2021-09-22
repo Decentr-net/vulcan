dd856714cc0a8546cad995b4a8f1fa7a921806d2
Vladislav Mikitich <vmikitich@gmail.com>
2021-09-22 18:45:18 +0300

    2nd broadcaster

--- internal/service/service.go
+++ internal/service/service.go
@@ -48,18 +48,27 @@ type Service interface {
 type service struct {
 	storage storage.Storage
 	sender  mail.Sender
-	bc      blockchain.Blockchain
+	btc     blockchain.Blockchain
+	bmc     blockchain.Blockchain
 
-	initialStakes int64
+	initialTestStakes int64
+	initialMainStakes int64
 }
 
 // New creates new instance of service.
-func New(storage storage.Storage, sender mail.Sender, b blockchain.Blockchain, initialStakes int64) Service {
+func New(
+	storage storage.Storage,
+	sender mail.Sender,
+	bt, bm blockchain.Blockchain,
+	initialTestNetStakes, initialMainNetStakes int64,
+) Service {
 	return &service{
-		storage:       storage,
-		sender:        sender,
-		bc:            b,
-		initialStakes: initialStakes,
+		storage:           storage,
+		sender:            sender,
+		btc:               bt,
+		bmc:               bm,
+		initialTestStakes: initialTestNetStakes,
+		initialMainStakes: initialMainNetStakes,
 	}
 }
 
@@ -134,8 +143,12 @@ func (s *service) Confirm(ctx context.Context, email, code string) error {
 		return ErrNotFound
 	}
 
-	if err := s.bc.SendStakes(req.Address, s.initialStakes); err != nil {
-		return fmt.Errorf("failed to send stakes to %s: %w", req.Address, err)
+	if err := s.btc.SendStakes(req.Address, s.initialTestStakes); err != nil {
+		return fmt.Errorf("failed to send stakes to %s on testnet: %w", req.Address, err)
+	}
+
+	if err := s.bmc.SendStakes(req.Address, s.initialMainStakes); err != nil {
+		return fmt.Errorf("failed to send stakes to %s on mainnet: %w", req.Address, err)
 	}
 
 	s.sender.SendWelcomeEmailAsync(ctx, req.Email)
