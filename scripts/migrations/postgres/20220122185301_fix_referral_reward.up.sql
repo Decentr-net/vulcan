7f775a3c04d69594d09189c3e802f775585adf98
Andrei Schneider <megaherz@bk.ru>
2022-01-28 18:52:47 +0300

    rewards biging (#43)

---
+++ scripts/migrations/postgres/20220122185301_fix_referral_reward.up.sql
@@ -0,0 +1,46 @@
+DROP FUNCTION referral_tracking_sender_stats (addr VARCHAR, since INTERVAL);
+
+ALTER TABLE referral_tracking
+    ALTER COLUMN sender_reward TYPE BIGINT,
+    ALTER COLUMN receiver_reward TYPE BIGINT;
+
+CREATE FUNCTION referral_tracking_sender_stats(addr VARCHAR, since INTERVAL)
+    RETURNS TABLE
+            (
+                registered INT,
+                installed  INT,
+                confirmed  INT,
+                reward     BIGINT
+            )
+AS
+$$
+BEGIN
+    RETURN QUERY
+        SELECT COALESCE(
+                       (SELECT COUNT(*)
+                        FROM referral_tracking
+                        WHERE sender = addr
+                          AND CASE WHEN since IS NULL THEN TRUE ELSE registered_at > NOW() - since END),
+                       0)::INT AS registered,
+               COALESCE(
+                       (SELECT COUNT(*)
+                        FROM referral_tracking
+                        WHERE sender = addr
+                          AND installed_at IS NOT NULL
+                          AND CASE WHEN since IS NULL THEN TRUE ELSE registered_at > NOW() - since END),
+                       0)::INT AS installed,
+               COALESCE(
+                       (SELECT COUNT(*)
+                        FROM referral_tracking
+                        WHERE sender = addr
+                          AND confirmed_at IS NOT NULL
+                          AND CASE WHEN since IS NULL THEN TRUE ELSE registered_at > NOW() - since END),
+                       0)::INT AS confirmed,
+               COALESCE(
+                       (SELECT SUM(COALESCE(sender_reward, 0))
+                        FROM referral_tracking
+                        WHERE sender = addr
+                          AND CASE WHEN since IS NULL THEN TRUE ELSE registered_at > NOW() - since END),
+                       0)::BIGINT AS reward;
+END;
+$$ LANGUAGE 'plpgsql';
